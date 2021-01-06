0f5c1c00b7218b9ff98fc65c91a8c113af5729ca
JenniferYang2020 <jenniferyang1990@hotmail.com>
2021-01-06 16:43:33 +0800

    FirstFile

---
+++ demo.js
@@ -0,0 +1,8 @@
+let box=document.getElementsByClassName("box")[0];
+
+window.onload=function(){
+    let box=document.getElementsByClassName("box")[0];
+    box.addEventListener('click',() =>{
+        alert('This is a test code')
+    })
+}
